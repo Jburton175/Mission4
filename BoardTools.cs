4f5543b1fa130d15dc0036d01fd8f27844d9f011
JBurton175 <jburton175@gmail.com>
2025-01-25 12:55:10 -0700

    add documentation framework

---
+++ BoardTools.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace Mission4
+{
+    internal class BoardTools
+    {
+
+        //Receive the “board” array from the driver class
+        //Contain a method that prints the board based on the information passed to it
+        //Contain a method that receives the game board array as input and returns if there is a winner and who it was
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Threading.Tasks;
+
+
+// Welcome the user to the game
+System.Console.WriteLine("Welcome to Tic-Tac-Toe!");
+
+// create gameboard array to store play choices
+// Ask each player in turn for their choice and update the game board array
+// Print the board by calling the method in the supporting class
+// Check for a winner by calling the method in the supporting class
+// notify the players when a win has occurred and which player won the game
+
