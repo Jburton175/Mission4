3ec0059c51d0603fa738f8d2729c7bd15168dbfe
JBurton175 <jburton175@gmail.com>
2025-01-29 09:28:32 -0700

    updated program.cs with tic-tac-toe game logic

--- Program.cs
+++ Program.cs
@@ -1,12 +1,66 @@
 ﻿using System.Threading.Tasks;
-
-
+using Mission4;
+    
 // Welcome the user to the game
 System.Console.WriteLine("Welcome to Tic-Tac-Toe!");
 
 // create gameboard array to store play choices
-// Ask each player in turn for their choice and update the game board array
-// Print the board by calling the method in the supporting class
-// Check for a winner by calling the method in the supporting class
-// notify the players when a win has occurred and which player won the game
+char [] board = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
+bool gameEnd = false;
+
+char player = 'X'; // every game starts with player x
+
+int playerInput = 0;
+
+int guesses = 0;
+
+
+printBoard(board);
+
+do
+{
+    // Ask each player in turn for their choice and update the game board array
+    System.Console.WriteLine("Player " + player + ", where do you want to place an " + player + "?");
+    //playerInput = System.Console.ReadLine();
+    while ((!int.TryParse(System.Console.ReadLine(), out playerInput) || playerInput <= 0) && (!Array.Exists(board, c => c == playerInput)))
+    {
+        System.Console.WriteLine("Please enter a number that has not been chosen yet, 1 through 9.");
+    }
+
+    board[playerInput-1] = player;
+
+
+    // set new player
+    if (player == 'X')
+    {
+        player = 'O';
+    }
+    else if (player == 'O')
+    {
+        player = 'X';
+    }
+
+    guesses++;
+
+    // Print the board by calling the method in the supporting class
+    printBoard(board);
+
+    // Check for a winner by calling the method in the supporting class notify the players when a win has occurred and which player won the game
+    gameWinner(board);
+
+    
+
+    //gameEnd = false;
+
+    if (guesses == 9 && gameEnd == false)
+    {
+        gameEnd = true;
+        System.Console.WriteLine("Cat's Game!");
+    }
+}
+
+while (gameEnd == false) ;
+
+
+System.Console.WriteLine("Game Over");
